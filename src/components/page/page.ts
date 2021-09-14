97a9a041ee122bb38ea3e8f0c72c5d0ff87fab70
seanstainability <seanstainability@gmail.com>
2021-09-14 13:03:36 +0900

    Add: PageComponent, ImageComponent 공통 부분 리팩토링

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,11 +1,7 @@
-export class PageComponent {
-  private readonly element: HTMLUListElement;
+import { BaseComponent } from "../component.js";
+
+export class PageComponent extends BaseComponent<HTMLUListElement> {
   constructor() {
-    this.element = document.createElement("ul");
-    this.element.setAttribute("class", "page");
-    this.element.textContent = "page component";
-  }
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
+    super('<ul class="page">page component!</ul>');
   }
 }
